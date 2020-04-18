01ac930876a56263a3f22b60f4493a9cee634e7d
icruz3 <59888011+icruz3@users.noreply.github.com>
2020-04-17 15:41:38 -1000

    Completed up to segmented control IBAction named

--- HackwichNine/ViewController.swift
+++ HackwichNine/ViewController.swift
@@ -10,11 +10,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var segmentedControl: UISegmentedControl!
+    
+    @IBOutlet weak var myLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        self.myLabel.text=""
     }
 
 
+    @IBAction func segmentedControlPressed(_ sender: Any) {
+        
+        
+        
+    }
+    
+    
 }
 
